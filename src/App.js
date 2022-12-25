671800a862bd72cf65911147e9432c360b4b66b5
krrong <khk9664@naver.com>
2022-12-25 22:44:28 +0900

    feat: array, object state 변경하는 법

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집';
-  let [글제목, 추천변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
 
   function 함수() {
@@ -16,6 +16,13 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
       <div className='list'>
         <h4>{ 글제목[0] } <span onClick={ ()=> 따봉변경(따봉+1) }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
