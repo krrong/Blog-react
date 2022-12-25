166dc5cccca6d4b47a4b891af0edcf5233e1bd1a
krrong <khk9664@naver.com>
2022-12-25 22:32:15 +0900

    feat: onClick, state 변경방법 구현

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 추천변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 함수() {
+    따봉변경(따봉+1)
+  }
 
   return (
     <div className="App">
@@ -12,11 +17,11 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={ ()=> 따봉변경(따봉+1) }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[1] }</h4>
+        <h4>{ 글제목[1] } <span onClick={ 함수 }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
